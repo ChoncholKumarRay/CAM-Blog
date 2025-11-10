70abc6137187a06f905525253ad6c6760ac10a7b
ChoncholKumarRay <fusitivechonchol@gmail.com>
2025-11-11 01:57:06 +0600

    fix: Showing Cloudinary image in the BlogDetail Page featured image

--- src/components/blogDetail/BlogImage.jsx
+++ src/components/blogDetail/BlogImage.jsx
@@ -1,13 +1,18 @@
+import PlaceholderImage from "../../assets/placeholder.jpg";
 const BlogImage = ({ featuredImage, title }) => {
   if (!featuredImage) return null;
 
-  const imageSrc = featuredImage.startsWith("data:image")
-    ? featuredImage
-    : `data:image/jpeg;base64,${featuredImage}`;
-
   return (
     <img
-      src={imageSrc}
+      src={
+        featuredImage.public_id
+          ? `https://res.cloudinary.com/${
+              import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "your-cloud-name"
+            }/image/upload/w_1024,c_fill,f_auto,q_auto,dpr_auto/${
+              featuredImage.public_id
+            }`
+          : { PlaceholderImage }
+      }
       alt={title}
       className="w-full rounded-lg object-cover max-h-[500px]"
     />
